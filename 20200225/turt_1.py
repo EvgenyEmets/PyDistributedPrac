881a3725be291a79b66923e6b7f0426f4080cdd4
Evgeny <you@example.com>
2020-03-03 02:01:30 +0300

    Fixed 2-nd alg

--- 20200225/turt_1.py
+++ 20200225/turt_1.py
@@ -2,28 +2,22 @@ from random import *
 from turtle import *
 from math import *
 
+def stop(x, y):
+	global flg
+	flg = False
 
-
-#setworldcoordinates(-470,-346,470,346)
+flg = True
 t1 = Turtle()
-
+t1.getscreen().onclick(stop)
 t1.shape("turtle")
 t1.pensize(3)
 t1.speed(0)
-"""t1.penup()
-t1.goto(300,300)
-t1.pendown()
-for i in range(4):
-	t1.right(90)
-	t1.fd(600)"""
 t1.penup()
-t1.goto(0,0)
 t2 = t1.clone()
 t2.hideturtle()
 t1.pendown()
-print(screensize())
 size = (window_width(), window_height())
-while True:
+while flg:
 	deg, sec = randrange(-60,60), randrange(200)
 	t2.circle(deg, sec)
 	if size[0] != window_width() and size[1] != window_height() and \
