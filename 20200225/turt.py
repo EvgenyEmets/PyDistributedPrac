e2720f72bd9f6060f91810abc6db6c50462dd223
Evgeny <you@example.com>
2020-03-03 01:12:46 +0300

    Fixed 1-st alg

--- 20200225/turt.py
+++ 20200225/turt.py
@@ -2,19 +2,12 @@ from random import *
 from turtle import *
 shape("turtle")
 pensize(3)
-penup()
-goto(300,300)
-pendown()
-for i in range(4):
-	right(90)
-	fd(600)
-penup()
-goto(0,0)
-pendown()
+speed(0)
 while True:
 	coord = pos()
-	if abs(coord[0]) >= 300 or abs(coord[1]) >= 300:
+	if abs(coord[0]) >= window_width() / 2 or abs(coord[1]) >= window_height() / 2:
 		undo()
+		continue
 	color(random(), random(), random())
 	circle(randrange(-60,60), randrange(200))
 	
